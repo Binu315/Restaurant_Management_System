3cd893851d76c5ec2e0fa37b1e8536cfea469897
Binu315 <meherafrozbinu@gmail.com>
2023-11-03 16:05:36 +0600

    restaurant management system

---
+++ welcome.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Restaurant Management System</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+<div class="container">
+    <h1>Welcome to the Restaurant Management System</h1>
+    <p>Please select an option below to get started:</p>
+    <ul>
+        
+       
+        <li><a href="menu.php">View Menu</a></li>
+        <li><a href="reservations.php">Reservation</a></li>
+        <li><a href="bill.php">Print bill</a></li>
+        <li><a href="inventory.php">Inventory</a></li>
+
+    </ul>
+</div>
+</body>
+</html>
+
